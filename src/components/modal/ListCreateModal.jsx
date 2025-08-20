9b1108d898fe4adcd8e29f23d6fdbaf2e44e7e7f
Alttodev <alttodev@gmail.com>
2025-08-20 14:21:45 +0530

    update

--- src/components/modal/ListCreateModal.jsx
+++ src/components/modal/ListCreateModal.jsx
@@ -10,7 +10,7 @@ export function CustomerDialog() {
     <Dialog open={isOpen} onOpenChange={(open) => !open && closeModal()}>
       <DialogContent className="max-w-[525px] [&_button]:cursor-pointer">
         <DialogHeader>
-          <DialogTitle className="text-emerald-600">
+          <DialogTitle className="text-emerald-600 flex justify-center">
             {id ? "Update" : "Create"}
           </DialogTitle>
         </DialogHeader>
