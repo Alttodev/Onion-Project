d1588efca92e0ecc0fccd76241abbc605e5b13c6
Alttodev <alttodev@gmail.com>
2025-08-26 22:45:20 +0530

    update

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {error} from "../assets/404_image.png"
 
 function ErrorPage() {
   return (
     <div className="flex bg-white items-center justify-center min-h-screen ">
       <Link to="/home">
         <img
-          src="/src/assets/404_image.png"
+          src={error}
           alt="404_image"
           className="h-70 w-100 object-cover"
         />
